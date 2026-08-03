b465ea5fd2319cc301ae42fe124f7df7cd916cde
ademan <ademan28@iCloud.com>
2026-08-03 22:58:30 +0900

    敵ごとの動き傾向作成

--- team/Assets/Scripts/Enemyfolder/EnemyData.cs
+++ team/Assets/Scripts/Enemyfolder/EnemyData.cs
@@ -10,5 +10,23 @@ public class EnemyData : ScriptableObject
     public int EnemyAttackDamage;//エネミー攻撃力
     public int EnemyDefense;//エネミー防御力
 
+    [Header("HP高い時の傾向")]
+    public int highHpCloseWeakWeight;
+    public int highHpCloseStrongWeight;
+    public int highHpLongWeakWeight;
+    public int highHpLongStrongWeight;
+
+    [Header("HP中間時の傾向")]
+    public int midHpCloseWeakWeight;
+    public int midHpCloseStrongWeight;
+    public int midHpLongWeakWeight;
+    public int midHpLongStrongWeight;
+
+    [Header("HP低い時の傾向")]
+    public int lowHpCloseStrongWeight;
+    public int lowHpCloseWeakWeight;
+    public int lowHpLongStrongWeight;
+    public int lowHpLongWeakWeight;
+
 }
 
