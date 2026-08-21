7bf040ecd28348b0664678842bcdbe3a6ce59910
s <s>
2026-08-21 18:33:36 +0900

    チュートリアル作成中

---
+++ team/Assets/Scripts/Tutorial/UIText.cs
@@ -0,0 +1,58 @@
+using System.Collections.Generic;
+using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using UnityEngine.InputSystem;
+using TMPro;
+
+// UIを使っているのでこれを記入しましょう
+public class UIText : MonoBehaviour
+{
+    // nameText:喋っている人の名前
+    // talkText:喋っている内容やナレーション
+    public TextMeshProUGUI nameText;
+    public TextMeshProUGUI talkText;
+
+    public bool playing = false;
+    public float textSpeed = 0.1f;
+    void Start() { }
+    // クリックで次のページを表示させるための関数
+    public bool IsClicked()
+    {
+        if (Mouse.current != null && Mouse.current.leftButton.wasPressedThisFrame)
+            return true;
+
+        return false;
+    }
+    // ナレーション用のテキストを生成する関数
+    public void DrawText(string text)
+    {
+        nameText.text = "";
+        StartCoroutine(CoDrawText(text));
+    }
+    // 通常会話用のテキストを生成する関数
+    public void DrawText(string name, string text)
+    {
+        nameText.text = name + "\n「";
+        StartCoroutine("CoDrawText", text + "」");
+    }
+    // テキストがヌルヌル出てくるためのコルーチン
+    IEnumerator CoDrawText(string text)
+    {
+        playing = true;
+        float time = 0;
+        while (true)
+        {
+            yield return 0;
+            time += Time.deltaTime;
+            // クリックされると一気に表示
+            if (IsClicked()) break;
+            int len = Mathf.FloorToInt(time / textSpeed);
+            if (len > text.Length) break;
+            talkText.text = text.Substring(0, len);
+        }
+        talkText.text = text;
+        yield return 0;
+        playing = false;
+    }
+}
